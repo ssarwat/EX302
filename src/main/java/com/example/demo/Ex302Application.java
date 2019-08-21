2db7b0e7d95a3264bae7e0adfb993e736ea3393b
ssarwat <sumaiyah_sarwat@yahoo.com>
2019-08-21 09:26:01 -0400

    first commit

---
+++ src/main/java/com/example/demo/Ex302Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex302Application.class, args);
+    }
+
+}
